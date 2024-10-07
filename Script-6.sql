2c42d7ae39410939403f9998ca5ea5893009e86e
sangmin-seong <money679@naver.com>
2024-10-07 09:22:30 +0900

    no message

--- Script-6.sql
+++ Script-6.sql
@@ -165,3 +165,242 @@ COMMENT ON COLUMN "MANAGER"."AUTHORITY_MEMBER"   IS '회원권한';
 COMMENT ON COLUMN "MANAGER"."AUTHORITY_TRAINNER" IS '강사권한';
 
 
+SELECT 
+	EMP_ID,
+	EMP_NAME,
+	SALARY
+FROM EMPLOYEE;
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO BOARD_TYPE VALUES(1, '공지사항');
+INSERT INTO BOARD_TYPE VALUES(2, '자유게시판');
+INSERT INTO BOARD_TYPE VALUES(3, '정보게시판');
+COMMIT;
+
+------------------------------------------------------
+
+/* 게시글 번호 시퀀스 생성 */
+CREATE SEQUENCE SEQ_BOARD_NO NOCACHE;
+
+/* PL/SQL을 이용해서 BOARD 테이블에 샘플 데이터 삽입 */
+BEGIN 
+	FOR I IN 1..2000 LOOP
+		INSERT INTO "BOARD"
+		VALUES(
+			SEQ_BOARD_NO.NEXTVAL,
+			SEQ_BOARD_NO.CURRVAL || '번째 게시글',
+			SEQ_BOARD_NO.CURRVAL || '번째 게시글 내용입니다.',
+			DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+			1, 
+			CEIL( DBMS_RANDOM.VALUE(0,3) )
+		);
+	END LOOP;
+END;
+
+
+
+SELECT COUNT(*) FROM BOARD;
+
+COMMIT;
+
+-- 게시판종류별 샘플 데이터 개수 확인
+SELECT BOARD_CODE, COUNT(*)
+FROM BOARD
+GROUP BY BOARD_CODE
+ORDER BY BOARD_CODE ASC;
+
+---------------------------------
+/* 댓글 번호 시퀀스 생성 */
+CREATE SEQUENCE SEQ_COMMENT_NO NOCACHE;
+
+
+
+/* 댓글 테이블("COMMENT")에 샘플 데이터 삽입 */
+BEGIN
+	FOR I IN 1..3000 LOOP
+		INSERT INTO "COMMENT"
+		VALUES(
+			SEQ_COMMENT_NO.NEXTVAL,
+			SEQ_COMMENT_NO.CURRVAL || '번째 댓글',
+			DEFAULT, DEFAULT,
+			1,
+			CEIL( DBMS_RANDOM.VALUE(0, 1999) ),
+			NULL
+			);
+	END LOOP;
+END;
+
+SELECT COUNT(*) FROM "COMMENT";
+
+COMMIT;
+
+
+-- 게시글 번호 2000번에 작성된 댓글 개수 조회
+SELECT BOARD_NO, COUNT(*) 
+FROM "COMMENT"
+GROUP BY BOARD_NO
+ORDER BY BOARD_NO;
+
+
+/* 특정 게시판(BOARD_CODE)에
+ * 삭제 되지 않은 게시글 목록 조회
+ * 
+ * - 조회된 행 번호, : ROWNUM 또는 ROW_NUMBER() 이용
+ *   게시글 번호, 제목, 조회수, 작성일 : BOARD 테이블 컬럼
+ *   작성자 닉네임 : MEMBER 테이블에서 MEMBER_NICKNAME 컬럼
+ *   댓글수 : COMMENT 테이블에서 BOARD_NO 별 댓글 수(COUNT)
+ *   좋아요 갯수 : BOARD_LIKE 테이블에서 BOARD_NO 별 댓글 수(COUNT)
+ * 	 
+ * - 작성일은 몇 초/분/시간 전 또는 YYYY-MM-DD형식으로 조회
+ * 
+ * - 가장 최근 글이 제일 처음 조회
+ *   (SEQ_BOARD_NO를 이용, 내림차순 정렬)
+ */
+
+-- ROW_NUMBER() OVER (ORDER BY BOARD_NO ASC) AS "RNUM"
+ -->  BOARD_NO 오름차순으로 정렬 후 조회된 행의 번호를 지정
+
+-- 상관 서브쿼리 해석 순서
+-- 1) 메인 쿼리 1행 해석
+-- 2) 서브 쿼리에서 메인 쿼리 1행 조회 결과를 이용 -> 해석
+-- 3) 다시 메인 쿼리 해석
+
+
+SELECT
+	ROW_NUMBER() OVER(ORDER BY BOARD_NO ASC) AS "RNUM",
+	BOARD_NO,
+	BOARD_TITLE,
+	READ_COUNT,
+	MEMBER_NICKNAME,
+	(SELECT COUNT(*)
+	 FROM "COMMENT" C
+	 WHERE C.BOARD_NO = B.BOARD_NO ) AS "COMMENT_COUNT",
+	 
+	(SELECT COUNT(*)
+	 FROM "BOARD_LIKE" L
+	 WHERE L.BOARD_NO = B.BOARD_NO ) AS "LIKE_COUNT",
+	 
+	CASE 
+		WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 / 24 / 60
+		THEN FLOOR ( (CURRENT_DATE - BOARD_WRITE_DATE)
+									* 24 * 60 * 60) || '초 전'
+		
+		WHEN CURRENT_DATE-BOARD_WRITE_DATE < 1 / 24
+		THEN FLOOR ( (CURRENT_DATE - BOARD_WRITE_DATE)
+									* 24 * 60 ) || '분 전'
+
+		WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1
+		THEN FLOOR ( (CURRENT_DATE - BOARD_WRITE_DATE)
+									* 24) || '시간 전'
+									
+		ELSE TO_CHAR(BOARD_WRITE_DATE, 'YYYY-MM-DD')
+		
+	END AS BOARD_WRITE_DATE
+	 
+FROM
+	"BOARD" B
+JOIN
+	"MEMBER" M ON(B.MEMBER_NO = M.MEMBER_NO)
+WHERE
+	BOARD_DEL_FL = 'N'
+AND
+	BOARD_CODE = 1	
+ORDER BY RNUM DESC
+;
+
+INSERT INTO "BOARD"
+VALUES(
+	SEQ_BOARD_NO.NEXTVAL,
+	SEQ_BOARD_NO.CURRVAL || '번째 게시글',
+	SEQ_BOARD_NO.CURRVAL || '번째 게시글 내용 입니다',
+	DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+	1, 
+	1
+);
+
+
+
+
+SELECT *
+FROM "BOARD";
+
+INSERT INTO "BOARD"
+		VALUES(SEQ_BOARD_NO.NEXTVAL, #{boardTitle}, #{boardContent},
+						DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+						#{memberNo}, #{boardCode}					 );
+
+
+SELECT SEQ_BOARD_NO.NEXTVAL FROM DUAL;
+
+-- -----------------------------------------------------------------
+-- 이미지 번호 생성용 시퀀스
+CREATE SEQUENCE SEQ_IMG_NO NOCACHE;
+
+/* 여러 행을 한 번에 INSERT하는 방법
+ * 
+ * 1. INSERT ALL 구문
+ * 2. INSERT + SUBQUERY(사용!)
+ * 
+ * 
+ * [문제점] 위에 두 방법 다 SEQUENCE를 직접 사용할 수 없다(불가!!!)
+ * 
+ * 
+ * */
+
+
+-- SEQ_IMG_NO 시퀀스의 다음 값을 반환하는 함수 생성
+CREATE OR REPLACE FUNCTION NEXT_IMG_NO
+
+-- 반환형
+RETURN NUMBER
+
+-- 사용할 변수
+IS IMG_NO NUMBER;
+
+BEGIN 
+	SELECT SEQ_IMG_NO.NEXTVAL 
+	INTO IMG_NO
+	FROM DUAL;
+
+	RETURN IMG_NO;
+END;
+
+
+
+
+INSERT INTO BOARD_IMG
+(
+	SELECT
+		NEXT_IMG_NO(),
+		'/images/board',	'원본명',	'변경명', 	1,	2000
+	FROM DUAL
+	
+	UNION ALL
+	
+	SELECT
+		NEXT_IMG_NO(),
+		'/images/board',	'원본명',	'변경명',	1,	2000
+	FROM DUAL
+);
+
+ROLLBACK;
+
+
+
+
+
+
+
+
+
+
+
